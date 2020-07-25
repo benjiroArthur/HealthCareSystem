969f3285ca68b7dee10eb1ce426868a66649406a
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-07-25 04:15:17 +0000

    update

--- app/Http/Controllers/PharmacyController.php
+++ app/Http/Controllers/PharmacyController.php
@@ -12,7 +12,7 @@ class PharmacyController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'verified']);
     }
 
     /**
--- app/Http/Controllers/PrescriptionController.php
+++ app/Http/Controllers/PrescriptionController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\Request;
 
 class PrescriptionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
     /**
      * Display a listing of the resource.
      *
