122b674c8b4bc2ce83449484dd4da0d56975387f
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-07-25 03:46:05 +0000

    update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,8 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 //'useTLS' => true,
-                'host' => '127.0.0.1',
-                'port' => 6002,
+                'host' => env('APP_URL'),
+                'port' => env('LARAVEL_WEBSOCKETS_PORT'),
                 'scheme' => 'http'
             ],
         ],
